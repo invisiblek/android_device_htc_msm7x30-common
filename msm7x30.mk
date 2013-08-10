2036ea7e55f9bffd84c59e5ee18cdf725b3e7228
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-08-10 02:03:46 -0500

    msm7x30: set bg app limit to half of default
    Change-Id: I49e7fc1d5b5e2487d613491d99cfe5d73dc7a92e

--- msm7x30.mk
+++ msm7x30.mk
@@ -87,6 +87,9 @@ PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=12
+
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
